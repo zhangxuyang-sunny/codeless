3d1499dbb1708446b77d488e44d8b81e12431dc9
zhangxuyang <240283142@qq.com>
2022-05-25 21:08:33 +0800

    core: 添加部署通知

--- scripts/redeploy-server.sh
+++ scripts/redeploy-server.sh
@@ -1,1 +1,5 @@
 pnpm --filter @low/server redeploy
+
+curl -X POST -H "Content-Type: application/json" \
+	-d '{"msg_type":"text","content":{"text":"x-server 成功重新部署"}}' \
+  https://open.feishu.cn/open-apis/bot/v2/hook/907d2be0-0929-4444-afbf-e1931257c5c8
